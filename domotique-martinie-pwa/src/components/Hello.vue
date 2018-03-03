61259c44ff3d6673d766d8cf435cac330f620cd4
Romain KOENIG <romain.koe@gmail.com>
2018-03-03 19:41:27 +0100

    first commit

---
+++ domotique-martinie-pwa/src/components/Hello.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="hello">
+      <div>
+        <el-button type="primary" v-on:click="getMessage">{{msg}}</el-button>
+      </div>
+    </div>
+    <ul v-if="posts && posts.length">
+      <li v-for="post of posts" v-bind:key="post.id">
+        <p><strong>{{post.title}}</strong></p>
+        <p>{{post.body}}</p>
+      </li>
+    </ul>
+
+    <ul v-if="errors && errors.length">
+      <li v-for="error of errors" v-bind:key="error.id">
+        {{error.message}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'cliquez moi',
+      posts: [],
+      errors: [],
+    };
+  },
+  methods: {
+    getMessage: function getMessage() {
+      axios.get('http://jsonplaceholder.typicode.com/posts').then((response) => {
+        this.posts = response.data;
+      }).catch((e) => {
+        this.errors.push(e);
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
